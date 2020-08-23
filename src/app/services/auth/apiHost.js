4d95e512ed37a85ce5625106c6bd30b1ae8b5ae0
Peter <threelephant@gmail.com>
2020-08-23 13:43:23 +0700

    For deployment

--- src/app/services/auth/apiHost.js
+++ src/app/services/auth/apiHost.js
@@ -1,6 +1,7 @@
-const host = process.env.HOST || "http://192.168.0.106"
-const port = process.env.PORT || 5000
+const host = "http://inventoryapi3.herokuapp.com" // process.env.HOST || "" || "http://192.168.0.106"
+// const port = process.env.PORT || 5000
 
-const api = `${host}:${port}/api`
+// const api = `${host}:${port}/api`
+const api = `${host}/api`
 
 export default api
