66eedf30e2018618ea628af8583c15455f9923da
Peter <threelephant@gmail.com>
2020-08-23 14:34:19 +0700

    Changed host

--- src/app/services/auth/apiHost.js
+++ src/app/services/auth/apiHost.js
@@ -1,4 +1,4 @@
-const host = "http://inventoryapi3.herokuapp.com" // process.env.HOST || "" || "http://192.168.0.106"
+const host = "http://inventory1api.herokuapp.com" // process.env.HOST || "" || "http://192.168.0.106"
 // const port = process.env.PORT || 5000
 
 // const api = `${host}:${port}/api`
